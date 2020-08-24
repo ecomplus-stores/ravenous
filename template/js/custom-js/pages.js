6d2db109cd974fa6975dd2d6eaeebc26f8f04730
Del Vieira <70026397+delvieira@users.noreply.github.com>
2020-08-24 20:06:31 -0300

    Update pages.js
    Mudar o texto 'produção' para  'confecção'

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,3 @@
 // Add your custom JavaScript for storefront pages here.
+  var confeccao = $('.product__production').html().replace('produção', 'confecção');
+  $('.product__production').html(confeccao);
