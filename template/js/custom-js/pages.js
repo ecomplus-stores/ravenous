d4f12f4e87648ac65e2272b704d7bf2e7ba22d24
Del Vieira <70026397+delvieira@users.noreply.github.com>
2020-08-24 21:18:03 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,5 +1,1 @@
 // Add your custom JavaScript for storefront pages here.
-$( document ).ready(function() {
-  var confeccao = $('.product__production').html().replace('produção', 'confecção');
-  $('.product__production').html(confeccao);
-});
